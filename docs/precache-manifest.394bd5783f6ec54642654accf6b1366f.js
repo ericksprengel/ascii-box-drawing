42b5061818307539d07d902f45c4ed023bb8c5b2
Erick Massa Sprengel <erick.sprengel@gmail.com>
2020-06-08 19:03:48 -0300

    demo v0

---
+++ docs/precache-manifest.394bd5783f6ec54642654accf6b1366f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12f4f6c5798b4c59e8063a020db2cc7",
+    "url": "/ascii-box-drawing/index.html"
+  },
+  {
+    "revision": "e3d8f64551d83a72f87e",
+    "url": "/ascii-box-drawing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87c64442b9f0e37492d9",
+    "url": "/ascii-box-drawing/static/js/2.b8a2e486.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/ascii-box-drawing/static/js/2.b8a2e486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d8f64551d83a72f87e",
+    "url": "/ascii-box-drawing/static/js/main.b512fead.chunk.js"
+  },
+  {
+    "revision": "50c40c217df152b44820",
+    "url": "/ascii-box-drawing/static/js/runtime-main.3e4061da.js"
+  }
+]);
