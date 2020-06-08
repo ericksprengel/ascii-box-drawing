d1a4762347ddc9a2d58a34d5b65f3cbfb670b7ea
Erick Massa Sprengel <erick.sprengel@gmail.com>
2020-06-08 18:59:46 -0300

    demo v0

---
+++ precache-manifest.5e7d046c9e78fdd110b9df2a36a98be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1115b3780dccd274c84143ac0f088f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fe822a9bfe71a06967e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87c64442b9f0e37492d9",
+    "url": "/static/js/2.b8a2e486.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.b8a2e486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe822a9bfe71a06967e",
+    "url": "/static/js/main.a7adb595.chunk.js"
+  },
+  {
+    "revision": "67479161cbb10a6de071",
+    "url": "/static/js/runtime-main.40acd08e.js"
+  }
+]);
